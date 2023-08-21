79b95611687fc49b05f30058426c097f50438f00
hasanify <hasanali.abps@gmail.com>
2023-08-21 21:46:22 +0530

    fix utc offset error in timestamp

--- app/notes/[id]/page.jsx
+++ app/notes/[id]/page.jsx
@@ -9,7 +9,12 @@ const Note = async ({ params }) => {
       <div className="p-8 mb-[140px] mx-auto break-words max-w-[800px] w-full bg-yellow-200 shadow-xl">
         <div className="mb-4 text-center border-b-2 border-b-slate-800/90 md:text-left">
           <h1 className="text-3xl">{note.title}</h1>
-          <p> {moment(note.created).format("MMM Do YYYY, h:mm a")}</p>
+          <p>
+            {" "}
+            {moment(note.created)
+              .utcOffset("+0530")
+              .format("MMM Do YYYY, h:mm a")}
+          </p>
         </div>
         <p>{note.content}</p>
       </div>
