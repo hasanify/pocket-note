d5b84753cea3e32d4f30ac6d4109299490337858
hasanify <hasanali.abps@gmail.com>
2023-08-21 21:48:03 +0530

    fix html semantics error

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import TakingNotes from "../public/images/Taking notes-pana.svg";
+import TakingNotes from "../public/images/landing-art.webp";
 import Link from "next/link";
 const Home = () => {
   return (
@@ -7,10 +7,10 @@ const Home = () => {
       <section className="text-gray-600 body-font">
         <div className="container flex flex-col items-center mx-auto md:flex-row">
           <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
-            <h1 className="mb-4 font-medium text-gray-900 title-font">
+            <div className="mb-4 font-medium text-gray-900 title-font">
               <h1 className="text-5xl sm:text-4xl">PocketNote</h1>
               <p className="text-xl">Your friendly neighborhood note app!</p>
-            </h1>
+            </div>
             <p className="mb-8 leading-relaxed">
               PocketNote, powered by Next.js and PocketBase, is the perfect
               note-taking solution for your ideas and thoughts. Organize, sync,
@@ -29,7 +29,7 @@ const Home = () => {
           <div className="w-72 lg:max-w-lg lg:w-full md:w-1/2">
             <Image
               alt="Illustration"
-              className="hidden object-cover object-center rounded md:block"
+              className="hidden object-cover object-center rounded pointer-events-none select-none -z-10 md:block"
               src={TakingNotes}
             />
           </div>
