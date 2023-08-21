0f41b19ba6cae7fdd25c4bc999f0b1d63fe7d494
hasanify <hasanali.abps@gmail.com>
2023-08-21 21:45:36 +0530

    set active tab in navbar on initial page load

--- app/(Components)/Navbar.jsx
+++ app/(Components)/Navbar.jsx
@@ -1,61 +1,64 @@
 "use client";
-import Link from "next/link";
-import { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const tabs = [
   { id: "home", label: "Home", href: "/" },
   { id: "notes", label: "Notes", href: "/notes" },
+  { id: "signup", label: "Sign Up", href: "/signup" },
 ];
 
 const Navbar = () => {
-  let currentTab = "";
-  if (typeof window !== "undefined") {
-    currentTab = tabs.find(
-      (tab) => window?.location?.pathname === tab.href
-    )?.id;
-  }
+  const [activeTab, setActiveTab] = useState("");
 
-  const [activeTab, setActiveTab] = useState(currentTab);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const currentTab = tabs.find(
+        (tab) => tab.href === window.location.pathname
+      )?.id;
+
+      if (currentTab) {
+        setActiveTab(currentTab);
+      }
+    }
+  }, []);
 
   return (
-    <nav className="gap-2 max-w-[1000px] mb-12 p-2 mx-auto text-center">
-      <Link
-        onClick={() => setActiveTab("home")}
-        href={"/"}
-        className="flex items-center justify-center gap-2 mb-2 md:absolute "
-      >
-        <Image
-          width={50}
-          height={50}
-          alt={"Logo"}
-          src={"/images/logo.png"}
-        ></Image>
-        <div className="text-2xl font-semibold">PocketNote</div>
-      </Link>
-      {tabs.map((tab) => (
+    <>
+      <nav className="gap-0 p-4 md:gap-2 max-w-[1000px] mx-auto text-center">
         <Link
-          key={tab.id}
-          onClick={() => setActiveTab(tab.id)}
-          className={`
-          p-4 text-xl font-semibold relative uppercase transition-all text-slate-900 
+          onClick={() => setActiveTab("home")}
+          href={"/"}
+          className="flex items-center justify-center gap-2 mb-2 md:absolute "
+        >
+          <Image width={50} height={50} alt={"Logo"} src={"/images/logo.png"} />
+          <div className="text-2xl font-semibold">PocketNote</div>
+        </Link>
+        {tabs.map((tab) => (
+          <Link
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`
+          px-2 md:px-4 text-xl font-semibold relative uppercase transition-all text-slate-900 
           ${activeTab === tab.id ? "" : "hover:text-slate-700"}
           `}
-          href={tab.href}
-        >
-          {activeTab === tab.id && (
-            <motion.div
-              transition={{ duration: 0.1 }}
-              layoutId={"active"}
-              className="absolute inset-0 py-1 mt-12 bg-yellow-200 rounded-full"
-            />
-          )}
+            href={tab.href}
+          >
+            {activeTab === tab.id && (
+              <motion.div
+                transition={{ duration: 0.1 }}
+                layoutId={"active"}
+                className="absolute inset-0 py-1 mt-6 bg-yellow-200 rounded-full"
+              />
+            )}
 
-          {tab.label}
-        </Link>
-      ))}
-    </nav>
+            {tab.label}
+          </Link>
+        ))}
+      </nav>
+    </>
   );
 };
 
